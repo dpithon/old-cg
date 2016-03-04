114c3e28b6cb3e8b29ee13360592a1761093171e
Denis Pithon <d.pithon@maine-et-loire.fr>
2016-03-04 11:44:40 +0100

    new function assert_is_mequal()

--- log.c
+++ log.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "vmath.h"
+
+#define HL   "\033[33m"
+#define NOHL "\033[0m"
+
 
 void fatal(const char *msg)
 {
-	fprintf(stderr, "FATAL: %s\n", msg);
+	fprintf(stderr, HL "FATAL: %s\n" NOHL, msg);
 	exit(1);
 }
 
 
 void warning(const char *msg)
 {
-	fprintf(stderr, "Warning: %s\n", msg);
+	fprintf(stderr, HL "Warning: %s\n" NOHL, msg);
 }
+
+
+#ifndef NDEBUG
+void assert_is_mequal(const struct matrix *m1, const struct matrix *m2)
+{
+	if (!is_mequal(m1, m2)) {
+		fprintf(stderr, HL "assert_is_mequal(m1(%p), m2(%p)) "
+			"failed!\n" NOHL, (void*)m1, (void*)m2);
+		print_matrix("m1", m1);
+		print_matrix("m2", m2);
+		exit(1);
+	}
+}
+#endif
--- log.h
+++ log.h
@@ -1,7 +1,17 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include "types.h"
+
 void fatal(const char*);
 void warning(const char*);
+
+#ifndef NDEBUG
+void assert_is_mequal(const struct matrix*, const struct matrix*);
+
+#else  /* ! NDEBUG */
+
+#define assert_is_mequal(m1, m2) ((void)0)((void)0)
+#endif /* ! NDEBUG */
 
 #endif /* LOG_H */
