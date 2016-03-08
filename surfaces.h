43cd7c25ea4d70f7e0c9306ccba16a86511539ea
Denis Pithon <denis.pithon@gmail.com>
2016-03-08 22:17:46 +0100

    update cylinder() declaration

--- surfaces.h
+++ surfaces.h
@@ -7,8 +7,7 @@ extern struct shape *plane(const struct coord*, const struct coord*);
 extern struct shape *sphere(const struct coord*, double);
 extern struct shape *paraboloid(const struct coord*, const struct coord*,
 				double, double, double, double);
-extern struct shape *cylinder(const struct coord*, const struct coord*,
-			      double, double);
-struct shape *cone(const struct coord*, const struct coord*, double);
+extern struct shape *cylinder(const struct coord*, const struct coord*, double);
+extern struct shape *cone(const struct coord*, const struct coord*, double);
 
 #endif /* SURFACES_H */
