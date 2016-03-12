0d313860ba80cfcc576602854c3a1880fd3429e3
Denis Pithon <denis.pithon@gmail.com>
2016-03-12 15:29:23 +0100

    add macros Vx, Vy, Vz, Sx, Sy, Sz

--- ray.h
+++ ray.h
@@ -3,6 +3,13 @@
 
 #include "vmath.h"
 
+#define Vx	(ray->v.x)
+#define Vy	(ray->v.y)
+#define Vz	(ray->v.z)
+#define Sx	(ray->s.x)
+#define Sy	(ray->s.y)
+#define Sz	(ray->s.z)
+
 struct ray {
 	struct coord s;
 	struct coord v;
