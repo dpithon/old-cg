aed156297feb920d689d2a5169415b78fd70700f
Denis Pithon <denis.pithon@gmail.com>
2016-03-12 16:46:27 +0100

    update sphere functions

--- sphere.c
+++ sphere.c
@@ -1,5 +1,5 @@
+#include <assert.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include "vmath.h"
 #include "types.h"
@@ -14,33 +14,30 @@
 
 struct sphere {
 	SHAPE_INF;
-	double radius;
+	double r;
+	double r2;
 };
+
+
+#define R2	((struct sphere*)s)->r2
 
 
 static bool sphere_intersect(struct ipoint *i, const struct ray *ray,
 			     const struct shape *s)
 {
-	double a, b, c, delta;
-	double r = ((struct sphere*) s)->radius;
+	double a, b, c, delta, k1, k2, sqrt_delta;
 
-	a = ray->v.x * ray->v.x + ray->v.y * ray->v.y + ray->v.z * ray->v.z;
-	b = 2. * (ray->v.x * ray->s.x + ray->v.y * ray->s.y + ray->v.z * ray->s.z);
-	c = ray->s.x * ray->s.x + ray->s.y * ray->s.y + ray->s.z * ray->s.z - r * r;
+	a = Vx * Vx + Vy * Vy + Vz * Vz;
+	b = 2. * (Vx * Sx + Vy * Sy + Vz * Sz);
+	c = Sx * Sx + Sy * Sy + Sz * Sz - R2;
 
 	delta = b * b - 4 * a * c;
 
-	if (double_equals(delta, 0.)) {
-		double k = -b / (2. * a);
+	if (delta < 0.) {
+		return false;
 
-		if (k > 0 && k < i->k) {
-			set_ipoint(i, s, FLAG_OUTSIDE, k);
-			return true;
-		}
 	} else if (delta > 0.) {
-		double k1, k2, sqrt_delta;
-
-		sqrt_delta = sqrtf(delta);
+		sqrt_delta = sqrt(delta);
 		k1 = (-b - sqrt_delta) / (2. * a);
 		k2 = (-b + sqrt_delta) / (2. * a);
 
@@ -51,27 +48,37 @@ static bool sphere_intersect(struct ipoint *i, const struct ray *ray,
 			set_ipoint(i, s, FLAG_INSIDE, k2);
 			return true;
 		}
+
+	} else {
+		k1 = -b / (2. * a);
+
+		if (k1 > 0 && k1 < i->k) {
+			set_ipoint(i, s, FLAG_OUTSIDE, k1);
+			return true;
+		}
 	}
 
 	return false;
 }
 
 
-struct shape *sphere(const struct coord *loc, double radius)
+struct shape *sphere(const struct coord *loc, double r)
 {
+	assert_is_point(loc);
+	assert(r > 0.);
+
 	struct sphere *sph = malloc(sizeof(struct sphere));
 
-	assert_is_point(loc);
-
-	sph->radius = radius;
 	sph->cs.o   = *loc;
 	sph->cs.i   = VectorI;
 	sph->cs.j   = VectorJ;
 	sph->cs.k   = VectorK;
 
 	transform(&sph->cs.o);
-
 	change_of_coord_mat(&sph->cs);
+
+	sph->r  = r;
+	sph->r2 = r * r;
 	sph->intersect  = sphere_intersect;
 	sph->paint      = default_painter;
 	sph->paint_data = 0;
