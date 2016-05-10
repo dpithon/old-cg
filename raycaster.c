a7b3c23a0a68dd281a6dd1bd4cc3f280fcaf8b14
Denis Pithon <d.pithon@maine-et-loire.fr>
2016-05-10 14:21:07 +0200

    sample animation

--- raycaster.c
+++ raycaster.c
@@ -9,8 +9,10 @@
 #include "sampler.h"
 #include "material.h"
 
-#define W	500
-#define H	500
+#include <stdio.h>
+
+#define W	192
+#define H	192
 
 
 #define CONES	12
@@ -33,7 +35,7 @@ void build_scene(void)
 		loc3.z = sin(nangle) * (RADIUS - 4.);
 
 		s = cone(&loc2, &loc1, 2.);
-		pattern_circle(s, OUTSIDE, .1, RGBLightGray, RGBDarkBlue);
+		pattern_circle(s, OUTSIDE, .4, RGBLightGray, RGBDarkBlue);
 		pattern_circle(s, INSIDE, .3, RGBOrange, RGBWhite);
 		add_shape(s);
 
@@ -44,59 +46,52 @@ void build_scene(void)
 	}
 
 	s = plane(&point_o, &vector_j);
-	pattern_checker(s, OVER, 5., RGBRed, RGBDarkGray);
+	pattern_checker(s, OVER, 10., RGBCornflowerBlue, RGBSnow);
+	pattern_checker(s, UNDER, 5., RGBMediumOrchid1, RGBGrey100);
 	add_shape(s);
 
 	s = sphere(&point_o, 4);
-	pattern_stripes(s, OUTSIDE, 0.8, RGBWhite, RGBDarkGray);
+	pattern_stripes(s, OUTSIDE, 0.8, RGBLightSteelBlue, RGBDarkSeaGreen4);
 	add_shape(s);
 
 }
-
-
-/*
-void build_scene2(void)
-{
-	struct shape *s;
-	struct coord apex = { 0, 10, 0, 1};
-
-	s = paraboloid(&point_o, &apex, 5.);
-	set_plain_color(s, OUTSIDE, RGBLightBlue);
-	set_plain_color(s, INSIDE, RGBDarkBlue);
-	add_shape(s);
-}
-
-
-void build_scene3(void)
-{
-	struct shape *s;
-	struct coord apex = { 0, 10, 0, 1};
-
-	s = cone(&point_o, &apex, 10.);
-	set_plain_color(s, OUTSIDE, RGBLightBlue);
-	set_plain_color(s, INSIDE, RGBDarkBlue);
-	add_shape(s);
-}
-*/
 
 
 int main()
 {
-	set_location(30, 30, 30);
-	set_target(0, 0, 0);
-	if (!init_pinhole(W, H, 45.)) {
-		fatal("failed to initialize pinhole camera");
-		return 1;
-	}
+	char filename[16];
+	int iter;
+	double x, y, z, angle, step, radius;
 
-	init_pixmap(W, H);
+	angle = 0.;
+	radius = 30.;
+	iter = 180;
+	step = 2. * M_PI / (double) iter;
 
 	build_scene();
-	prepare_shape_matrices(pinhole_coord_system());
 
-	for (int x = 0; x < W; x++)
-		for (int y = 0; y < H; y++)
-			sampler(x, y);
+	for (int n = 0; n < iter; n++, angle += step) {
+		x = cos(angle) * radius;
+		z = sin(angle) * radius;
+		y = sin(angle) * 45.;
+		set_location(x, y, z);
+		set_target(0, 0, 0);
+		if (!init_pinhole(W, H, 45.)) {
+			fatal("failed to initialize pinhole camera");
+			return 1;
+		}
 
-	return write_pixmap(FORMAT_PPM, "a.pnm");
+		init_pixmap(W, H);
+
+		prepare_shape_matrices(pinhole_coord_system());
+
+		for (int x = 0; x < W; x++)
+			for (int y = 0; y < H; y++)
+				sampler(x, y);
+
+		sprintf(filename, "a%03d.pnm", n);
+		write_pixmap(FORMAT_PPM, filename);
+	}
+
+	return 0;
 }
