cdbf3d7b75704c25fda36706d3f0951d7c5f90d3
Denis Pithon <d.pithon@maine-et-loire.fr>
2017-12-05 16:43:27 +0100

    use quadratic_solve()

--- src/shapes/sphere.c
+++ src/shapes/sphere.c
@@ -8,35 +8,28 @@
 static bool sphere_intersect(struct hit *i, const struct ray *ray,
 			     const struct shape *s)
 {
-	double a, b, c, delta, k1, k2, sqrt_delta;
+	struct quadratic q = {
+		.a = Vx * Vx + Vy * Vy + Vz * Vz,
+		.b = 2. * (Vx * Sx + Vy * Sy + Vz * Sz),
+		.c = Sx * Sx + Sy * Sy + Sz * Sz - R2
+	};
+	solve_quadratic(&q);
 
-	a = Vx * Vx + Vy * Vy + Vz * Vz;
-	b = 2. * (Vx * Sx + Vy * Sy + Vz * Sz);
-	c = Sx * Sx + Sy * Sy + Sz * Sz - R2;
-
-	delta = b * b - 4. * a * c;
-
-	if (delta < 0.) {
+	if (q.count <= 0) {
 		return false;
 
-	} else if (delta > 0.) {
-		sqrt_delta = sqrt(delta);
-		k1 = (-b - sqrt_delta) / (2. * a);
-		k2 = (-b + sqrt_delta) / (2. * a);
-
-		if (k1 > 0. && k1 < i->k) {
-			set_hit(i, s, ray, OUTSIDE, k1);
+	} else if (q.count == 2) {
+		if (q.k1 > 0. && q.k1 < i->k) {
+			set_hit(i, s, ray, OUTSIDE, q.k1);
 			return true;
-		} else if (k2 > 0. && k2 < i->k) {
-			set_hit(i, s, ray, INSIDE, k2);
+		} else if (q.k2 > 0. && q.k2 < i->k) {
+			set_hit(i, s, ray, INSIDE, q.k2);
 			return true;
 		}
 
 	} else {
-		k1 = -b / (2. * a);
-
-		if (k1 > 0. && k1 < i->k) {
-			set_hit(i, s, ray, OUTSIDE, k1);
+		if (q.k1 > 0. && q.k1 < i->k) {
+			set_hit(i, s, ray, OUTSIDE, q.k1);
 			return true;
 		}
 	}
